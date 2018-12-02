b31470eafad848b0bab8002073cdfdc36309c9f7
Steve Herrin <steve.herrin@gmail.com>
2018-12-02 09:34:11 -0800

    Day 2, Problem 2; need to think about if there's a more efficient way

--- day02/src/main.rs
+++ day02/src/main.rs
@@ -1,3 +1,5 @@
+#![allow(unused_doc_comments)]
+
 use std::collections::HashMap;
 use std::env;
 use std::fs;
@@ -5,7 +7,8 @@ use std::iter::FromIterator;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let filename = &args[1];
+    let task = &args[1];
+    let filename = &args[2];
 
     let ids: Vec<String> = Vec::from_iter(
         fs::read_to_string(filename)
@@ -13,10 +16,15 @@ fn main() {
             .split_whitespace()
             .map(|s| s.to_owned()),
     );
-    println!("{}", checksum(&ids));
+    match task.as_str() {
+        "checksum" => println!("{}", checksum(&ids)),
+        "common" => println!("{}", common_for_similar_ids(&ids)),
+        _ => panic!("Don't know how to '{}'", task),
+    }
 }
 
 fn count_letters(id: &String) -> HashMap<char, u64> {
+    /// Given an id, return a map of letter to number of occurrences in the id
     let mut counts: HashMap<char, u64> = HashMap::new();
     for c in id.chars() {
         let n_c = counts.entry(c).or_insert(0);
@@ -32,6 +40,7 @@ struct ChecksumItem {
 }
 
 fn id_to_checksum_item(id: &String) -> ChecksumItem {
+    /// Given an id, return a struct of the information needed to compute the checksum
     let mut item: ChecksumItem = ChecksumItem {
         contains_2: false,
         contains_3: false,
@@ -47,18 +56,38 @@ fn id_to_checksum_item(id: &String) -> ChecksumItem {
 }
 
 fn checksum(ids: &Vec<String>) -> u64 {
-    let mut n_2: u64 = 0;
-    let mut n_3: u64 = 0;
-    for id in ids {
-        let item_for_id = id_to_checksum_item(id);
-        if item_for_id.contains_2 {
-            n_2 += 1;
-        }
-        if item_for_id.contains_3 {
-            n_3 += 1;
+    /// Given ids, return the checksum of the set
+    let (n_2, n_3) = ids
+        .iter()
+        .map(|id| id_to_checksum_item(id))
+        .fold((0, 0), |(n_2, n_3), item| {
+            (n_2 + item.contains_2 as u64, n_3 + item.contains_3 as u64)
+        });
+    n_2 * n_3
+}
+
+fn are_similar(s1: &String, s2: &String) -> bool {
+    /// return if the two strings are similar (differing in at most 1 place)
+    let n_dif = Iterator::zip(s1.chars(), s2.chars())
+        .filter(|(c1, c2)| c1 != c2)
+        .count();
+    n_dif <= 1
+}
+
+fn common_for_similar_ids(ids: &Vec<String>) -> String {
+    /// return the common characters between two similar ids
+    for i in 1..ids.len() {
+        let s1 = &ids[i - 1];
+        for s2 in ids[i..].iter() {
+            if are_similar(&s1, &s2) {
+                return Iterator::zip(s1.chars(), s2.chars())
+                    .filter(|(c1, c2)| c1 == c2)
+                    .map(|(c1, _c2)| c1)
+                    .collect::<String>();
+            }
         }
     }
-    n_2 * n_3
+    panic!("No similar ids!");
 }
 
 #[cfg(test)]
@@ -174,6 +203,57 @@ mod tests {
 
         for ref case in &cases[..] {
             assert_eq!(case.output, checksum(&case.input))
+        }
+    }
+
+    #[test]
+    fn test_are_similar() {
+        struct TestCase {
+            output: bool,
+            input1: String,
+            input2: String,
+        }
+
+        let cases: Vec<TestCase> = vec![
+            TestCase {
+                input1: String::from("abcde"),
+                input2: String::from("axcye"),
+                output: false,
+            },
+            TestCase {
+                input1: String::from("fghij"),
+                input2: String::from("fguij"),
+                output: true,
+            },
+        ];
+
+        for ref case in &cases[..] {
+            assert_eq!(case.output, are_similar(&case.input1, &case.input2))
+        }
+    }
+
+    #[test]
+    fn test_common_for_similar_ids() {
+        struct TestCase {
+            output: String,
+            input: Vec<String>,
+        }
+
+        let cases: Vec<TestCase> = vec![TestCase {
+            input: vec![
+                String::from("abcde"),
+                String::from("fghij"),
+                String::from("klmno"),
+                String::from("pqrst"),
+                String::from("fguij"),
+                String::from("axcye"),
+                String::from("wvxyz"),
+            ],
+            output: String::from("fgij"),
+        }];
+
+        for ref case in &cases[..] {
+            assert_eq!(case.output, common_for_similar_ids(&case.input))
         }
     }
 }
