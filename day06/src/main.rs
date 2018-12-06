f632bf56ed3e83bac29c1ea608c98cf4217ea11d
Steve Herrin <steve.herrin@gmail.com>
2018-12-06 00:43:29 -0800

    Day 6, Problem 2

--- day06/src/main.rs
+++ day06/src/main.rs
@@ -20,12 +20,15 @@ fn main() {
 
     if task == "area" {
         println!("{}", max_interior_area(&points));
+    } else if task == "proximal" {
+        println!("{}", proximal_area(10000, &points));
     } else {
         panic!("Don't know how to '{}'", task);
     }
 }
 
 fn max_interior_area(points: &[Point]) -> i32 {
+    /// compute the maximum nearest-neighbor area that isn't infinite
     let x_lo = points.iter().map(|p| p.x).min().unwrap();
     let x_hi = points.iter().map(|p| p.x).max().unwrap();
 
@@ -35,31 +38,72 @@ fn max_interior_area(points: &[Point]) -> i32 {
     let mut area_around: HashMap<Point, i32> = HashMap::new();
     let mut uncontained: HashSet<Point> = HashSet::new();
 
+    // scan a grid that contains all points
     for y in y_lo..=y_hi {
         for x in x_lo..=x_hi {
+            // find the nearest neighbors to the grid spot
             let p = Point::new(x, y);
             let mut nearest_neighbors: Vec<(i32, Point)> = points
                 .iter()
                 .map(|other| (p.distance(*other), *other))
                 .collect();
+
+            // throw away ties
             nearest_neighbors.sort_by_key(|(d, _other)| *d);
             let (distance, nn) = nearest_neighbors[0];
             let next_distance = nearest_neighbors[1].0;
             if distance != next_distance {
                 let area = area_around.entry(nn).or_insert(0);
-                *area += 1;
+                *area += 1; // the grid point now adds to the area
             }
+
+            // if a point is the nearest neighbor to an edge, it
+            // will be a nearest neighbor out to infinity, so exclude it
             if y == y_lo || y == y_hi || x == x_lo || x == x_hi {
                 uncontained.insert(nn);
             }
         }
     }
+    // of all the regions, find the largest one that doesn't go to infinity
     area_around
         .iter()
         .filter(|(p, _area)| !uncontained.contains(p))
         .map(|(_p, area)| *area)
         .max()
         .unwrap()
+}
+
+fn proximal_area(max_distance: i32, points: &[Point]) -> i32 {
+    /// compute the area of the region inside which the sum distance to all points is under a max
+    let x_lo = points.iter().map(|p| p.x).min().unwrap();
+    let x_hi = points.iter().map(|p| p.x).max().unwrap();
+
+    let y_lo = points.iter().map(|p| p.y).min().unwrap();
+    let y_hi = points.iter().map(|p| p.y).max().unwrap();
+
+    // Since we're using Manattan distance, computing the distance
+    // to all points and summing them at every point is not particularly
+    // efficient.
+    // Better would be to keep track of sorted x and y coordinates of the points.
+    // If we move +1 x, then the distance to all points left increases by 1,
+    // and the distance to all points right decreases by 1, and so the
+    // total distance changes by the difference.
+    // That said, this is simple and seems to run fast enough.
+
+    let mut area: i32 = 0;
+    for y in y_lo..=y_hi {
+        for x in x_lo..=x_hi {
+            let p = Point::new(x, y);
+            let total_distance: i32 = points.iter().map(|other| p.distance(*other)).sum();
+            if total_distance < max_distance {
+                area += 1;
+                if y == y_lo || y == y_hi || x == x_lo || x == x_hi {
+                    panic!("The proximal region might extend past the bounding box of the points");
+                }
+            }
+        }
+    }
+    area
 }
 
 #[derive(Debug, PartialEq, Eq, Hash, Copy, Clone)]
@@ -168,6 +212,32 @@ mod tests {
 
         for ref case in &cases[..] {
             assert_eq!(case.output, max_interior_area(&case.input));
+        }
+    }
+
+    #[test]
+    fn test_proximate_area() {
+        struct TestCase {
+            points: Vec<Point>,
+            max_distance: i32,
+            output: i32,
+        }
+
+        let cases: Vec<TestCase> = vec![TestCase {
+            points: vec![
+                Point::new(1, 1),
+                Point::new(1, 6),
+                Point::new(8, 3),
+                Point::new(3, 4),
+                Point::new(5, 5),
+                Point::new(8, 9),
+            ],
+            max_distance: 32,
+            output: 16,
+        }];
+
+        for ref case in &cases[..] {
+            assert_eq!(case.output, proximal_area(case.max_distance, &case.points));
         }
     }
 }
