497368413e0c45b8d7aa19c4df0a3d8823cca048
Steve Herrin <steve.herrin@gmail.com>
2018-12-07 21:59:55 -0800

    Day 8, Problem 2

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -17,6 +17,8 @@ fn main() {
 
     if task == "meta" {
         println!("{}", sum_metadata(&tree));
+    } else if task == "value" {
+        println!("{}", node_value(&tree));
     } else {
         panic!("Don't know how to '{}'", task);
     }
@@ -56,6 +58,39 @@ fn _sum_metadata(tree: &[i32]) -> (i32, usize) {
     }
 }
 
+fn node_value(tree: &[i32]) -> i32 {
+    /// return the value of the root node of the tree
+    let (v, _) = _node_value(tree);
+    v
+}
+
+fn _node_value(tree: &[i32]) -> (i32, usize) {
+    /// recursive helper that returns both the value and how many items are consumed
+    let n_children = tree[0];
+    let n_metadata = tree[1] as usize;
+
+    if n_children == 0 {
+        (
+            tree[2..(2 + n_metadata)].iter().sum(),
+            2 + (n_metadata as usize),
+        )
+    } else {
+        let mut children: Vec<i32> = Vec::new();
+        let mut idx = 2;
+        for _i in 0..n_children {
+            let (child_value, child_idx) = _node_value(&tree[idx..]);
+            children.push(child_value);
+            idx += child_idx;
+        }
+        let mut value: i32 = 0;
+        for child in tree[idx..(idx + n_metadata)].iter() {
+            value += children.get((*child - 1) as usize).unwrap_or(&0);
+        }
+        idx += n_metadata;
+        (value, idx)
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -80,6 +115,29 @@ mod tests {
 
         for ref case in &cases[..] {
             assert_eq!(case.output, sum_metadata(&case.input));
+        }
+    }
+
+    #[test]
+    fn test_root_value() {
+        struct TestCase {
+            input: Vec<i32>,
+            output: i32,
+        }
+
+        let cases: Vec<TestCase> = vec![
+            TestCase {
+                input: vec![1, 2, 0, 1, 7, 1, 1],
+                output: 14, // i think
+            },
+            TestCase {
+                input: vec![2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2],
+                output: 66,
+            },
+        ];
+
+        for ref case in &cases[..] {
+            assert_eq!(case.output, node_value(&case.input));
         }
     }
 }
