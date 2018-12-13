0d86564778971455956213549fbb61bafd2db696
Steve Herrin <steve.herrin@gmail.com>
2018-12-12 19:05:28 -0800

    Day 12 Cleanup
    - error handling on the IO
    - make test not fragile
    - general cleanup

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -12,33 +12,8 @@ fn main() {
     let task = &args[1];
     let filename = &args[2];
 
-    let f = File::open(filename).unwrap();
-    let mut reader = BufReader::new(f);
-    let mut line = String::new();
+    let mut row = read_file(filename).expect("Couldn't read file!");
 
-    reader.read_line(&mut line).expect("Problem reading file");
-    let parts: Vec<&str> = line.split(": ").collect();
-    let state = parse_str(parts[1]);
-
-    let mut rules: HashMap<[Pot; 5], Pot> = HashMap::new();
-    for mut rule_line in reader.lines() {
-        let rl = rule_line.unwrap();
-        if rl.trim().is_empty() {
-            continue;
-        }
-        let parts: Vec<&str> = rl.trim().split(" => ").collect();
-        let rule_str = parts[0];
-        let mut key = [Pot::None; 5];
-        assert!(rule_str.len() == 5);
-        for (i, c) in rule_str.chars().enumerate() {
-            key[i] = parse_char(c);
-        }
-        let value = parse_str(parts[1].trim())[0];
-
-        rules.insert(key, value);
-    }
-
-    let mut row = Row::new(&state, rules);
     if task == "sum" {
         for _ in 0..20 {
             row.tick();
@@ -75,6 +50,52 @@ fn main() {
     }
 }
 
+#[derive(Debug)]
+enum ReadError {
+    IO(std::io::Error),
+    Malformed(String),
+}
+
+impl From<std::io::Error> for ReadError {
+    fn from(err: std::io::Error) -> ReadError {
+        ReadError::IO(err)
+    }
+}
+
+fn read_file(filename: &str) -> Result<Row, ReadError> {
+    let f = File::open(filename)?;
+    let mut reader = BufReader::new(f);
+    let mut line = String::new();
+
+    reader.read_line(&mut line)?;
+    let state_str = line.split(": ").nth(1);
+    if state_str.is_none() {
+        return Result::Err(ReadError::Malformed(String::from(
+            "State line seems malformed",
+        )));
+    }
+    let state = parse_str(state_str.unwrap());
+
+    let mut rules: HashMap<[Pot; 5], Pot> = HashMap::new();
+    for mut maybe_line in reader.lines() {
+        let rule_line = maybe_line?;
+        if rule_line.trim().is_empty() {
+            continue;
+        }
+        let rule_to_final: Vec<&str> = rule_line.trim().split(" => ").collect();
+        let rule_str = rule_to_final[0];
+        let mut rule_key = [Pot::None; 5];
+        assert!(rule_str.len() == 5);
+        for (i, c) in rule_str.chars().enumerate() {
+            rule_key[i] = parse_char(c);
+        }
+        let rule_value = parse_str(rule_to_final[1].trim())[0];
+
+        rules.insert(rule_key, rule_value);
+    }
+    Ok(Row::new(&state, rules))
+}
+
 #[derive(Debug, PartialEq, Eq, Hash, Clone, Copy)]
 enum Pot {
     Plant,
@@ -100,7 +121,6 @@ impl Row {
         let len = self.pots.len();
         let mut new_pots = vec![Pot::None; SIZE];
         for i in 2..(len - 2) {
-            //TODO check limits
             let five_pots = &self.pots[(i - 2)..=(i + 2)];
             new_pots[i] = match self.rules.get(five_pots) {
                 None => Pot::None,
@@ -132,7 +152,10 @@ fn parse_str(s: &str) -> Vec<Pot> {
     s.trim().chars().map(parse_char).collect()
 }
 
+#[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn test_sum_of_plants() {
         struct TestCase {
@@ -282,8 +305,6 @@ mod tests {
         ];
 
         let expected = vec![
-            Pot::None,
-            Pot::None,
             Pot::Plant,
             Pot::None,
             Pot::None,
@@ -315,6 +336,14 @@ mod tests {
 
         let mut row = Row::new(&state, rules);
         row.tick();
-        assert_eq!(expected, row.pots);
+        let region_with_plants: Vec<Pot> = row
+            .pots
+            .iter()
+            .skip(OFFSET)
+            .take(expected.len())
+            .cloned()
+            .collect();
+
+        assert_eq!(expected, region_with_plants);
     }
 }
