31fdb0b3a35d54c445cd01b1d32fdb8b3b0462f1
Steve Herrin <steve.herrin@gmail.com>
2018-12-23 12:42:37 -0800

    Day 23, yet another failed approach to problem 2

--- day23/src/main.rs
+++ day23/src/main.rs
@@ -1,10 +1,11 @@
 #[macro_use]
 extern crate lazy_static;
+extern crate priority_queue;
 extern crate regex;
 
+use priority_queue::PriorityQueue;
 use std::cmp::max;
 use std::cmp::min;
-use std::collections::HashMap;
 use std::env;
 use std::fs;
 use std::num::ParseIntError;
@@ -85,51 +86,6 @@ impl Bot {
     }
 }
 
-/// Given a list of points and ranges, compute the interval(s) where they overlap most
-fn find_max_overlaps(pos_range: &[(i64, i64)]) -> Vec<(i64, i64)> {
-    // first work how how many ranges start and end at each position
-    let mut n_start: HashMap<i64, i64> = HashMap::new();
-    let mut n_end: HashMap<i64, i64> = HashMap::new();
-    for (p, r) in pos_range {
-        let ds = n_start.entry(p - r).or_insert(0);
-        *ds += 1;
-        let de = n_end.entry(p + r).or_insert(0);
-        *de -= 1;
-    }
-
-    // merge those together, sort so we look at starts before ends
-    let mut deltas = n_start.drain().collect::<Vec<(i64, i64)>>();
-    deltas.append(&mut n_end.drain().collect::<Vec<(i64, i64)>>());
-    deltas.sort_by_key(|(x, d)| (*x, -*d));
-
-    // first pass through and figure out the largest number we have in an overlap
-    let mut n_max_overlap = 0;
-    let mut csum = 0;
-    for (_x, delta) in &deltas {
-        csum += delta;
-        n_max_overlap = max(csum, n_max_overlap);
-    }
-
-    // then find all ranges that have that amount of overlap
-    let mut max_overlaps: Vec<(i64, i64)> = Vec::new();
-    let mut csum = 0;
-    let mut start: Option<i64> = None;
-    for (x, delta) in &deltas {
-        csum += delta;
-        if csum == n_max_overlap {
-            if let None = start {
-                start = Some(*x);
-            }
-        } else {
-            if let Some(s) = start {
-                max_overlaps.push((s, *x));
-            }
-            start = None;
-        }
-    }
-    max_overlaps
-}
-
 fn strongest_range(bots: &[Bot]) -> usize {
     let strongest = bots.iter().max_by_key(|b| b.r).expect("No bots");
     bots.iter()
@@ -137,7 +93,7 @@ fn strongest_range(bots: &[Bot]) -> usize {
         .count()
 }
 
-#[derive(Debug)]
+#[derive(Debug, Eq, PartialEq, Hash)]
 struct Volume {
     x_lo: i64,
     x_hi: i64,
@@ -148,89 +104,83 @@ struct Volume {
 }
 
 impl Volume {
-    fn n_points(&self) -> i64 {
-        (self.x_hi - self.x_lo + 1) * (self.y_hi - self.y_lo + 1) * (self.z_hi - self.z_lo + 1)
-    }
-}
-
-#[derive(Debug)]
-struct PointsOfInterest {
-    xs: Vec<i64>,
-    ys: Vec<i64>,
-    zs: Vec<i64>,
-}
-
-impl PointsOfInterest {
-    fn new() -> PointsOfInterest {
-        PointsOfInterest {
-            xs: Vec::new(),
-            ys: Vec::new(),
-            zs: Vec::new(),
+    fn new(x_lo: i64, x_hi: i64, y_lo: i64, y_hi: i64, z_lo: i64, z_hi: i64) -> Volume {
+        Volume {
+            x_lo,
+            x_hi,
+            y_lo,
+            y_hi,
+            z_lo,
+            z_hi,
         }
     }
-    fn n_points(&self) -> usize {
-        self.xs.len() * self.ys.len() * self.zs.len()
+    fn new_zero() -> Volume {
+        Volume::new(0, 0, 0, 0, 0, 0)
     }
-}
-
-fn find_search_points(bots: &[Bot]) -> PointsOfInterest {
-    let xs = bots.iter().map(|b| (b.x, b.r)).collect::<Vec<(i64, i64)>>();
-    let x_overlaps = find_max_overlaps(&xs);
-    let ys = bots.iter().map(|b| (b.y, b.r)).collect::<Vec<(i64, i64)>>();
-    let y_overlaps = find_max_overlaps(&ys);
-    let zs = bots.iter().map(|b| (b.z, b.r)).collect::<Vec<(i64, i64)>>();
-    let z_overlaps = find_max_overlaps(&zs);
-
-    println!("{:?}", x_overlaps);
-    println!("{:?}", y_overlaps);
-    println!("{:?}", z_overlaps);
-
-    let mut pois = PointsOfInterest::new();
-    for (x, r) in &xs {
-        if x_overlaps
-            .iter()
-            .any(|(x_lo, x_hi)| *x_lo <= x - r && x - r <= *x_hi)
-        {
-            pois.xs.push(*x - *r);
+    fn containing_bots(bots: &[Bot]) -> Volume {
+        let mut vol = Volume::new_zero();
+        for bot in bots {
+            vol.x_lo = min(vol.x_lo, bot.x - bot.r);
+            vol.y_lo = min(vol.y_lo, bot.y - bot.r);
+            vol.z_lo = min(vol.z_lo, bot.z - bot.r);
+            vol.x_hi = max(vol.x_hi, bot.x + bot.r);
+            vol.y_hi = max(vol.y_hi, bot.y + bot.r);
+            vol.z_hi = max(vol.z_hi, bot.z + bot.r);
         }
-        if x_overlaps
-            .iter()
-            .any(|(x_lo, x_hi)| *x_lo <= x + r && x + r <= *x_hi)
-        {
-            pois.xs.push(*x + *r);
-        }
+        vol
     }
-    for (y, r) in &ys {
-        if y_overlaps
-            .iter()
-            .any(|(y_lo, y_hi)| *y_lo <= y - r && y - r <= *y_hi)
-        {
-            pois.ys.push(*y - *r);
-        }
-        if y_overlaps
-            .iter()
-            .any(|(y_lo, y_hi)| *y_lo <= y + r && y + r <= *y_hi)
-        {
-            pois.ys.push(*y + *r);
-        }
+    fn n_in_range(&self, bots: &[Bot]) -> usize {
+        bots.iter()
+            .filter(|b| b.x - b.r <= self.x_hi && self.x_lo <= b.x + b.r)
+            .filter(|b| b.y - b.r <= self.y_hi && self.y_lo <= b.y + b.r)
+            .filter(|b| b.z - b.r <= self.z_hi && self.z_lo <= b.z + b.r)
+            .count()
     }
-    for (z, r) in &zs {
-        if z_overlaps
-            .iter()
-            .any(|(z_lo, z_hi)| *z_lo <= z - r && z - r <= *z_hi)
-        {
-            pois.zs.push(*z - *r);
-        }
-        if z_overlaps
-            .iter()
-            .any(|(z_lo, z_hi)| *z_lo <= z + r && z + r <= *z_hi)
-        {
-            pois.zs.push(*z + *r);
-        }
+    fn is_point(&self) -> bool {
+        self.x_lo == self.x_hi && self.y_lo == self.y_hi && self.z_lo == self.z_hi
     }
-
-    println!("{:?}", pois);
-    pois
+    fn min_corner_distance(&self) -> i64 {
+        min(self.x_lo.abs(), self.x_hi.abs())
+            + min(self.y_lo.abs(), self.y_hi.abs())
+            + min(self.z_lo.abs(), self.z_hi.abs())
+    }
+    fn octants(&self) -> Vec<Volume> {
+        let x_mid = (self.x_lo + self.x_hi) / 2;
+        let y_mid = (self.y_lo + self.y_hi) / 2;
+        let z_mid = (self.z_lo + self.z_hi) / 2;
+        let mut octants = vec![
+            Volume::new(self.x_lo, x_mid, self.y_lo, y_mid, self.z_lo, z_mid),
+            Volume::new(self.x_lo, x_mid, self.y_lo, y_mid, z_mid + 1, self.z_hi),
+            Volume::new(self.x_lo, x_mid, y_mid + 1, self.y_hi, self.z_lo, z_mid),
+            Volume::new(self.x_lo, x_mid, y_mid + 1, self.y_hi, z_mid + 1, self.z_hi),
+            Volume::new(x_mid + 1, self.x_hi, self.y_lo, y_mid, self.z_lo, z_mid),
+            Volume::new(x_mid + 1, self.x_hi, self.y_lo, y_mid, z_mid + 1, self.z_hi),
+            Volume::new(x_mid + 1, self.x_hi, y_mid + 1, self.y_hi, self.z_lo, z_mid),
+            Volume::new(
+                x_mid + 1,
+                self.x_hi,
+                y_mid + 1,
+                self.y_hi,
+                z_mid + 1,
+                self.z_hi,
+            ),
+        ];
+        // by adding 1 to ensure non-overlapping octants,
+        // we might have created some invalid ones; strip them out
+        octants.retain(|v| v.x_lo <= v.x_hi && v.y_lo <= v.y_hi && v.z_lo <= v.z_hi);
+        //println!("before: {:?}", octants);
+        // don't duplicate itself
+        octants.retain(|v| {
+            v.x_lo != self.x_lo
+                || v.x_hi != self.x_hi
+                || v.y_lo != self.y_lo
+                || v.y_hi != self.y_hi
+                || v.z_lo != self.z_lo
+                || v.z_hi != self.z_hi
+        });
+        //println!("after: {:?}", octants);
+        octants
+    }
 }
 
 fn n_in_range(x: i64, y: i64, z: i64, bots: &[Bot]) -> usize {
@@ -239,48 +189,44 @@ fn n_in_range(x: i64, y: i64, z: i64, bots: &[Bot]) -> usize {
         .count()
 }
 
-fn best_in_volume(bots: &[Bot], volume: &Volume) -> (usize, i64) {
+fn best_spot(bots: &[Bot]) -> i64 {
+    let mut queue: PriorityQueue<Volume, usize> = PriorityQueue::new();
+    let full_volume = Volume::containing_bots(bots);
+    let full_n = full_volume.n_in_range(bots);
+    queue.push(full_volume, full_n);
+    let mut i = 0;
     let mut best_n = 0;
-    let mut best_d = i64::max_value();
-    for x in volume.x_lo..=volume.x_hi {
-        for y in volume.y_lo..=volume.y_hi {
-            for z in volume.z_lo..=volume.z_hi {
-                let n = n_in_range(x, y, z, bots);
-                //println!("    ({}, {}, {}) {}", x, y, z, n);
-                if n > best_n {
-                    best_n = n;
-                    best_d = x.abs() + y.abs() + z.abs();
-                } else if n == best_n {
-                    let d = x.abs() + y.abs() + z.abs();
-                    best_d = min(d, best_d); // if a tie, pick the one nearest the origin
-                }
+    let mut best: Vec<Volume> = Vec::new();
+    while let Some((vol, n)) = queue.pop() {
+        if i % 100000 == 0 {
+            println!("queue {}", queue.len());
+            //println!("{} {:?}", n, vol);
+        }
+        i += 1;
+        for oct in vol.octants() {
+            let oct_n = oct.n_in_range(bots);
+            if oct.is_point() && oct_n > best_n {
+                best_n = oct_n;
+            }
+            //println!("  {} {:?}", oct_n, oct);
+            if oct_n >= best_n {
+                queue.push(oct, oct_n);
             }
         }
-    }
-    (best_n, best_d)
-}
-
-fn best_point(bots: &[Bot], pois: &PointsOfInterest) -> (usize, i64) {
-    let mut best_n = 0;
-    let mut best_d = i64::max_value();
-    for x in &pois.xs {
-        for y in &pois.ys {
-            for z in &pois.zs {
-                let n = n_in_range(*x, *y, *z, bots);
-                //println!("    ({}, {}, {}) {}", x, y, z, n);
-                if n > best_n {
-                    best_n = n;
-                    best_d = x.abs() + y.abs() + z.abs();
-                } else if n == best_n {
-                    let d = x.abs() + y.abs() + z.abs();
-                    best_d = min(d, best_d); // if a tie, pick the one nearest the origin
-                }
-            }
+        if n >= best_n && vol.is_point() {
+            best.push(vol);
         }
     }
-    (best_n, best_d)
+    best.sort_by_key(|v| {
+        (
+            n_in_range(v.x_lo, v.y_lo, v.z_lo, bots),
+            -v.min_corner_distance(),
+        )
+    });
+    best.last().expect("No best spot").min_corner_distance()
 }
 
+/*
 fn best_spot(bots: &[Bot]) -> i64 {
     let pois = find_search_points(bots);
     println!("{} points", pois.n_points());
@@ -289,6 +235,7 @@ fn best_spot(bots: &[Bot]) -> i64 {
     println!("best was {} at {}", n, d);
     d
 }
+*/
 
 #[cfg(test)]
 mod tests {
@@ -321,22 +268,5 @@ mod tests {
             "pos=<10,10,10>, r=5".parse::<Bot>().unwrap(),
         ];
         assert_eq!(36, best_spot(&bots));
-    }
-
-    #[test]
-    fn test_max_overlap() {
-        let pos_ranges = vec![
-            (10, 2),
-            (12, 2),
-            (16, 4),
-            (14, 6),
-            (50, 200),
-            (10, 5),
-            (17, 2),
-            (18, 1),
-            (19, 1),
-        ];
-        let overlaps = find_max_overlaps(&pos_ranges);
-        assert_eq!(vec![(12, 12), (18, 19)], overlaps);
     }
 }
